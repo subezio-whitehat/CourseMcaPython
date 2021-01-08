ea76174034c58019c5185e01fe75227e8978a54e
subezio-whitehat <74660211+subezio-whitehat@users.noreply.github.com>
2021-01-09 00:24:53 +0530

    Delete vowel.py

--- cycle1/vowel.py
+++
@@ -1,7 +0,0 @@
-x=input("\nEnter the character:")
-if(x=='a' or x=='A' or x=='e' or x=='E'
-   or x=='i' or x=='I' or x=='o' or x=='O'
-   or x=='u' or x=='U'):
-    print(x,"is a vowel")
-else:
-    print(x,"is a consonent")
