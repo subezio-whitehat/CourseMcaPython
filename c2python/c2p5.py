747ec68dca673dabb6e263cceec0ae58a2674ca4
subezio-whitehat <subinsabu554@gmail.com>
2021-01-12 00:21:46 +0530

    python cycle2

--- c2python/c2p5.py
+++ c2python/c2p5.py
@@ -1,9 +1,13 @@
 lst=[]
 subs="a"
+count=0
 n=int(input("\nEnter the number of frst names:"))
 for n in range(0,n):
     fname=input("\nEnter the name:")
     lst.append(fname)
-count=lst.count(subs)
+for x in lst:
+    if subs in x:
+        count=count+1
+        
 print("\nThe list is:",lst)
 print("\nThe count of a in list is:",count)
