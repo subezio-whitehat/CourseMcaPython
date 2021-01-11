077b8e51f050a7df73ff55fb53f7a0ddc7432ae8
subezio-whitehat <subinsabu554@gmail.com>
2021-01-11 18:51:17 +0530

    python cycle2

--- c2python/c2p1.py
+++ c2python/c2p1.py
@@ -2,6 +2,6 @@ final=int(input("Enter the year"))
 current=int(input("\nEnter the current year"))
 x=current
 while(x<=final):
-    if(x%4)==0:
+    if((x%4==0) and (x%100!=0)) or (x%400==0):
         print("\nleap year=:",x)
     x=x+1
