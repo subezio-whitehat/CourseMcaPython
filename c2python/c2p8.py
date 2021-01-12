bd8e96c8ea256fb3b0e73a1c1456291e0fbfb5a9
subezio-whitehat <subinsabu554@gmail.com>
2021-01-12 23:23:29 +0530

    python cycle2

---
+++ c2python/c2p8.py
@@ -0,0 +1,3 @@
+s=input("\nEnter a string:")
+newstr=s[-1] + s[1:-1] + s[0]
+print(newstr)
---
+++ c2python/c2p9.py
@@ -0,0 +1,3 @@
+r=int(input("\nEnter the radius of circle:"))
+area=3.14*r*r
+print("\nThe area of the circle with radius %d is %f!" %(r,area))
