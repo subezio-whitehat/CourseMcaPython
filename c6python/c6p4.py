0b0fc87f3e5b244b92a906d64202afc028780d81
subezio-whitehat <74660211+subezio-whitehat@users.noreply.github.com>
2021-03-16 16:12:08 +0530

    Update c6p4.py

--- c6python/c6p4.py
+++ c6python/c6p4.py
@@ -1,3 +1,5 @@
+#Write a Python program to read specific columns of a given CSV file and print the content
+of the columns
 import csv
 with open('D:\depart.csv', newline='') as csvfile:
  data = csv.DictReader(csvfile)
